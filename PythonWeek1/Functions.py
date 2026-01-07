dd105dc002b06b1bfbb79ed927e44126ea9a91c7
Kelsey Morrison <kelsey.linton@revature.com>
2026-01-07 14:42:35 -0500

    Added arrays and lambda functions

--- PythonWeek1/Functions.py
+++ PythonWeek1/Functions.py
@@ -46,3 +46,51 @@ local_variable()
 MyCat = "Wonton"
 
 #Built-in: defautl python methods and all the keywords live here. can be accessed from anywhere
+
+
+#Arrays are not technically built into Python, we have to import the array module
+#contain elements of the same type, stored sequentially and indexed from 0
+#array(typecode, initializer)
+
+# typecode: This is a single character that specifies the type of data that will be stored in the array. For example:
+# 'i': Signed integer (4 bytes)
+# 'f': Floating point (4 bytes)
+# 'd': Double precision floating point (8 bytes)
+# 'b': Signed integer (1 byte)
+# 'u': Unicode character (2 bytes)
+# 'l': Signed long integer (4 bytes) (platform-dependent)
+# initializer: This is an optional parameter that initializes the array with elements. It can be a list, a tuple, or any iterable containing the initial values for the array.
+
+import array
+IntArray = array.array( 'i', [1, 2, 14, 6, 3, 99])
+
+print(IntArray[2])
+subset = IntArray[0:4]
+print(subset)
+
+for i in IntArray:
+    print(i)
+
+#find the length
+print(len(IntArray))
+
+#sorts in ascending order
+print(sorted(IntArray))
+
+
+#Lambda functions AKA Anonymous functions
+#small, single-line functions that can have any number of arguments, but only one expression
+#lambda args: expression
+
+add = lambda x, y: x + y
+print(add(3, 4))
+
+Pokedex = [
+    ("Bulbasaur", 1),
+    ("Lapras", 131),
+    ("Eevee", 133),
+    ("Cubone", 104),
+    ("Gengar", 94)
+]
+Pokedex.sort(key=lambda x: x[1], reverse = True)
+print(Pokedex)
