f45f5753c040ee0cc77f885e2b2ff46affab1d92
Kelsey Morrison <kelsey.linton@revature.com>
2026-01-08 11:24:30 -0500

    added file handling sorta

---
+++ PythonWeek1/FileHandling.py
@@ -0,0 +1,23 @@
+#File handling refers to the process of creating, opening, reading, writing, and deleting files using Python
+#Python has built-in functions and methods to make this easy
+
+#Important to know for data persistence and processing
+#automation of many real-world tasks
+#handling large data sets
+
+#open() is the key function and takes 2 parameters; filename and mode
+#4 modes available:
+#"r" - read; the default mode for reading; throws an error if file does not exist
+#"a" - append; creates a file if it does not exist
+#"w" - write; creates a new file if it does not exist
+#"x" - create; returns an error if file does not exist
+
+#We can also specify if the file should be handled as binary or text using "b" and "t" respectively
+
+
+
+#myFile = open('./resources/my-file.txt', "w")
+
+#print(x.read())
+#y = open("myOtherFile.txt", "rt")
+
